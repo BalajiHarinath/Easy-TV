dcb8e418cc9ec80932ca84706d2c370146df04fd
BalajiHarinath <89400019+BalajiHarinath@users.noreply.github.com>
2022-05-19 11:29:16 +0530

    Removed the commented code

--- src/components/HeaderNav/HeaderNav.jsx
+++ src/components/HeaderNav/HeaderNav.jsx
@@ -11,20 +11,6 @@ export const HeaderNav = () => {
       <h3>
         Easy<span className="title-header text-3xl font-semibold"> TV</span>
       </h3>
-      {/* <div className="flex flex-grow-1 flex-justify-center">
-        <div className="flex flex-align-center">
-          <button className="btn-transparent flex flex-align-center">
-            <span className="material-icons-round btn-search flex flex-align-center pdl-0-5 pdr-0-5 text-lg">
-              search
-            </span>
-          </button>
-          <input
-            className="input-search pdl-0-5 pdr-0-5"
-            type="text"
-            placeholder="What are you looking for..."
-          />
-        </div>
-      </div> */}
       <div className="flex flex-gap-2">
         <Link
           className="btn-transparent flex flex-column flex-justify-center flex-align-center"
